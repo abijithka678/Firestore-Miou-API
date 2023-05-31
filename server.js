5724bd6a5e77b9fdadd4f31149f857a592a51c0d
Abijith K A <abijithka02@gmail.com>
2023-05-31 12:20:00 +0530

    change path to pet/:userId

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
 
 
 // POST API endpoint
-app.post('/:userId', (req, res) => {
+app.post('pet/:userId', (req, res) => {
   const userId = req.params.userId;
   const data = req.body;
 
