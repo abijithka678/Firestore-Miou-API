218a020ec74ea6a42b093d1960726dcfdb7b27f6
Abijith K A <abijithka02@gmail.com>
2023-05-31 10:52:16 +0530

    Need to change key.json got not named so that path cannot detect and not get worked

--- server.js
+++ server.js
@@ -4,20 +4,46 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/api/users', (req, res) => {
-    return res.status(200).send('hello world!');
-    // Handle GET request for retrieving users
-    // You can perform necessary operations and send back the response
-  });
-  
-  app.post('/api/users', (req, res) => {
-    // Handle POST request for creating a new user
-    // Access the data sent in the request body using req.body
-  });
-  
-  // Add more routes as needed
+var admin = require("firebase-admin");
 
-  const port = 3000;
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://miou-api-default-rtdb.firebaseio.com"
+});
+
+
+
+
+
+app.get('/api/users', (req, res) => {
+  const db = admin.firestore();
+  const usersCollection = db.collection('pet');
+
+  usersCollection.get()
+    .then((snapshot) => {
+      const users = [];
+      snapshot.forEach((doc) => {
+        users.push(doc.data());
+      });
+      res.json(users);
+    })
+    .catch((error) => {
+      console.error('Error getting users:', error);
+      res.status(500).send('Error getting users');
+    });
+});
+
+
+app.post('/api/users', (req, res) => {
+  // Handle POST request for creating a new user
+  // Access the data sent in the request body using req.body
+});
+
+// Add more routes as needed
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
